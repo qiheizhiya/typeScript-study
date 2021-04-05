80aa681fd338f055b94c08c6261608fa550dde8e
qiheizhiya <953136447@qq.com>
2021-04-05 15:09:45 +0800

    完成

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,54 @@
 
 // console.log(filter(arr, n => n % 2 !== 0))
 
-import { ArrayHelper } from './ArrayHelper'
-const helper = new ArrayHelper([3, 4, 5, 6, 7])
+// import { ArrayHelper } from './ArrayHelper'
+// const helper = new ArrayHelper([3, 4, 5, 6, 7])
 
-helper.shuffle()
+// helper.shuffle()
+
+
+// interface hasNameProperty {
+//     name: string
+// }
+
+// function nameToUpperCase<T extends hasNameProperty>(obj: T): T {
+//     obj.name = obj.name.split(" ").map(s => s[0].toUpperCase() + s.substr(1)).join(" ")
+//     return obj
+// }
+
+// const o = {
+//     name: 'jie jie',
+//     age: 18
+// }
+
+// const nO = nameToUpperCase(o)
+// console.log(nO)
+
+
+// function mixinArray<T, K> (arr1: T[], arr2: K[]): (T | K)[] {
+//     if (arr1.length !== arr2.length) throw new Error('数组长度不相等')
+//     let result: (T | K)[] = []
+//     for (let i = 0; i < arr1.length; i++) {
+//         result.push(arr1[i])
+//         result.push(arr2[i])
+//     }
+//     return result
+// }
+
+import { Dictionary } from './dictionary'
+
+const dict = new Dictionary<string, number>()
+
+dict.add('1', 1)
+dict.add('2', 2)
+console.log(dict.size)
+dict.forEach((k, v) => {
+    console.log(k, ':', v)
+})
+
+dict.delete('1')
+console.log(dict.size)
+
+dict.clear()
+
+console.log(dict.size)
