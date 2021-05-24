fd4f650d89a5d19afb31d4db65ee523ad83702d6
qiheizhiya <953136447@qq.com>
2021-05-24 23:49:54 +0800

    学装饰器

--- src/Descriptor.ts
+++ src/Descriptor.ts
@@ -1,40 +1,24 @@
 type constructor = new (...args: any[]) => Object
-export function classDescriptor(description: string) {
-    return function (target: constructor) {
-        target.prototype.$classDescription = description
-    }
-}
 
-export function propDescriptor(description: string) {
-    return function (target: any, propName: string) {
-        if (!target.$propDescriptions) {
-            target.$propDescriptions = []
-        }
-        target.$propDescriptions.push({
-            propName,
-            description
-        })
-    }
+const key = Symbol('key')
+export function descriptor(description: string) {
+    return Reflect.metadata(key, description)
 }
 
 export function printObj (obj: any) {
-    if (obj.$classDescription) {
-        console.log(obj.$classDescription)
+    const cons = Object.getPrototypeOf(obj)
+    // 输出类的名字
+    if (Reflect.hasMetadata(key, cons)) {
+        console.log(Reflect.getMetadata(key, cons))
     } else {
-        console.log(Object.getPrototypeOf(obj).constructor.name)
+        console.log(cons.constructor.name)
     }
-    if (!obj.$propDescriptions) {
-        obj.$propDescriptions = []
-    }
-    for (const key in obj) {
+    for (const k in obj) {
         // 属性名称
-        if (obj.hasOwnProperty(key)) {
-            const prop = obj.$propDescriptions.find((p: any) => p.propName === key)
-            if (prop) {
-                console.log(`\t${prop.description}:${obj[key]}`)
-            } else {
-                console.log(`\t${key}:${obj[key]}`)
-            }
+        if (Reflect.hasMetadata(key, obj, k)) {
+            console.log(`\t${Reflect.getMetadata(key, cons, k)}:${obj[k]}`)
+        } else {
+            console.log(`\t${k}:${obj[k]}`)
         }
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,43 @@
-import { classDescriptor, printObj, propDescriptor } from './Descriptor'
+// import { printObj, descriptor } from './Descriptor'
+import { IsNotEmpty, validate } from 'class-validator'
+import 'reflect-metadata'
 
-@classDescriptor('用户')
-class User {
-    @propDescriptor('账户')
+// @descriptor('用户')
+// class User {
+//     @descriptor('账户')
+//     loginId!: string
+
+//     @descriptor('密码')
+//     loginPwd!: string
+// }
+
+
+// const u = new User()
+// u.loginId = '123'
+// u.loginPwd = '123'
+// printObj(u)
+
+
+// @Reflect.metadata('a', '一个类')
+// class A {
+//     @Reflect.metadata('prop', '一个参数')
+//     prop1: string | undefined
+// }
+// const aaa = new A()
+// a.prop1 = '123'
+// console.log(Reflect.getMetadata('a', A))
+// console.log(Reflect.getMetadata('prop', aaa, 'prop1'))
+
+class RegUser {
+    @IsNotEmpty({ message: '账号不能为空' })
     loginId!: string
-
-    @propDescriptor('密码')
     loginPwd!: string
+    age!: number
+    gender!: '男' | '女'
 }
 
+const post = new RegUser()
 
-const u = new User()
-u.loginId = '123'
-u.loginPwd = '123'
-printObj(u)
+validate(post).then(res => {
+    console.log(res)
+})
